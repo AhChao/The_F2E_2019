ba68b344d46e6a4c1f20bbb8e2888ad4b3e07412
Steven Chao <gash612@gmail.com>
2019-07-21 01:22:15 +0700

    FreeCell_Function Shell Update

--- 2.FreeCell/js/main.js
+++ 2.FreeCell/js/main.js
@@ -2,11 +2,15 @@ var deck = [];
 //var blackSymbol = ['♣','♦','♥','♠'];
 var blackSymbol = ['♣','♦','♥','♠'];
 var whiteSymbol = ['♧','♢','♡','♤'];
+var initColumn  =  [[]];
+var actionQueue = [[]]; 
 function init()
 {
+	actionQueue = [[]];
 	deckinit();
 	shuffle();
 	deal();
+	startTimer();
 }
 
 function deckinit()
@@ -15,6 +19,11 @@ function deckinit()
 	for(var i=0; i<52 ;i++)
 	{
 		deck[i] = i;
+	}
+	initColumn =[[]];
+	for(var i=0; i<8;i++)
+	{
+		initColumn[i] = [];
 	}
 }
 
@@ -34,12 +43,13 @@ function shuffle() {
 function deal()
 {
 	var columnLimit = [7,6];
-	for(var i = 1; i<=8;i++)
+	for(var i = 0; i<8;i++)
 	{
 		for(var j=0;j<columnLimit[i%2];j++)
 		{
-			var card = deck.pop();
-			drawPoker(card,"initColumn"+i);
+			var cardnum = deck.pop();
+			drawPoker(cardnum,"initColumn"+i);
+			initColumn[i].push(cardnum);
 		}
 	}
 }
@@ -50,5 +60,56 @@ function drawPoker(cardnum,targetArea)
 	var num = cardnum%13+1;
 	var color = "#000000";
 	if(suit==1||suit==2) color = "#FF0000";
-	d3.select("#"+targetArea).append("text").text(blackSymbol[suit]+num+",").attr("style","color:"+color+";") ;
+	d3.select("#"+targetArea).append("text").text(blackSymbol[suit]+num+",")
+	.attr("onclick","selectCard(this.id)")
+	.attr("style","color:"+color+";")
+	.attr("id","card"+cardnum) ;
+}
+
+function selectCard(cardnumId)
+{
+	var cardnum = cardnumId.split("card")[1];
+	var area = checkArea(cardnum);
+	console.log(area);
+}
+
+function checkArea(cardnum)
+{
+	for(var col in initColumn)
+	{
+		for(var card in initColumn[col])
+		{
+			if(initColumn[col][card]==cardnum)
+				return "initColumn"+col;
+		}
+	}
+	return "Error Action";//area name
+}
+
+function vaildateMoving(fromArea,targetArea)
+{
+
+}
+
+function selectArea(selectedArea)
+{
+}
+
+function selectChecking()//for init col multi sel
+{
+}
+
+function invaildAlert()
+{
+	alert("Invaild Moving!");
+}
+
+function undo()
+{
+
+}
+
+function startTimer()
+{
+
 }
